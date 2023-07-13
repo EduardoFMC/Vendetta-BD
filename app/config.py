26a2921b20ad19d9e7031eaf7f43dfb81c9ea549
Eduardo Ferreira <eduardo.ferreira.mc.edu@gmail.com>
2023-07-13 06:21:13 -0300

    config

--- app/config.py
+++ app/config.py
@@ -3,12 +3,21 @@ import os
 #db = MySQLdb.connect(host="localhost", user="root", passwd="12345", db="avalia_unb")
         #cursor = db.cursor()
 
-class DevelopmentConfig():
+
+""" class DevelopmentConfig():
     DEBUG = True
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
     MYSQL_PASSWORD = 'password'
+    MYSQL_DB = 'mydb' """
+
+class DevelopmentConfig():
+    DEBUG = True
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = ''
+    MYSQL_PASSWORD = ''
     MYSQL_DB = 'mydb'
+
 
 config = {
     'development': DevelopmentConfig
